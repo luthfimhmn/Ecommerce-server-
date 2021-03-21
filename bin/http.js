6b038c8f07539a0392479ff9f0b109e20b296272
Luthfi Muhaimin <luthfimhmn@gmail.com>
2021-03-21 16:24:52 +0700

    fixing port

--- bin/http.js
+++ bin/http.js
@@ -1,4 +1,4 @@
 const app = require('../app')
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Listening to port ', port))
+app.listen(port, () => console.log('Listening to port ', PORT))
